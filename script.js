24c15722ff99ef59d801e728ecec659e65d86644
sudo-CodeBoyBebop <42tjhenry@gmail.com>
2023-11-19 16:03:41 -0800

    implement data types

--- script.js
+++ script.js
@@ -1,127 +1,10 @@
-
-
-// let computerChoice = getComputerChoice();
-// let playerChoice = getPlayerSelection();
-
-
-
-
-
-
-
-
-// game();
-// game();
-
-
-
-
-
-
-
-
-// function declarations
-
-
-// gameplay functions
-// function getRandomNum() {
-//    let randNum = Math.floor(Math.random() * 3);
-//    return randNum;
-// }
-
-// gameplay functions
-// function getRandomNum() {
-//    let randNum = Math.floor(Math.random() * 3);
-//    return randNum;
-// }
-
-// function getComputerChoice() {
-//     let randNum = getRandomNum();
-//     let answer;
-//     if(randNum == 0) {
-//         answer = "rock";
-//     } else if (randNum == 1) {
-//         answer = "paper";
-//     } else if (randNum == 2) {
-//         answer = "scissors";
-//     }
-//         return answer;
-//     };
-// function getComputerChoice() {
-//     let randNum = getRandomNum();
-//     let answer;
-//     if(randNum == 0) {
-//         answer = "rock";
-//     } else if (randNum == 1) {
-//         answer = "paper";
-//     } else if (randNum == 2) {
-//         answer = "scissors";
-//     }
-//         return answer;
-//     };
-    
-// function getPlayerSelection() {
-//    let choice = prompt().toLowerCase();
-//    return choice;
-// }
-// function getPlayerSelection() {
-//    let choice = prompt().toLowerCase();
-//    return choice;
-// }
-
-// function playRound() {
-//    let result;
-//     let computerChoice = getComputerChoice();
-//     let playerChoice = getPlayerSelection();
-// function playRound() {
-//    let result;
-//     let computerChoice = getComputerChoice();
-//     let playerChoice = getPlayerSelection();
-
-//         if(computerChoice == playerChoice) {
-//             result = "Tie";
-//         } else if (computerChoice == "rock" && playerChoice == "paper"
-//          || computerChoice == "paper" && playerChoice == "scissors" 
-//          || computerChoice == "scissors" && playerChoice == "rock") {
-//             result = "Players Wins!";
-//         }  else if (computerChoice == "rock" && playerChoice == "scissors"
-//         || computerChoice == "paper" && playerChoice == "rock" 
-//         || computerChoice == "scissors" && playerChoice == "paper") {
-//            result = "Computer Wins!";
-//         }; 
-//         if(computerChoice == playerChoice) {
-//             result = "Tie";
-//         } else if (computerChoice == "rock" && playerChoice == "paper"
-//          || computerChoice == "paper" && playerChoice == "scissors" 
-//          || computerChoice == "scissors" && playerChoice == "rock") {
-//             result = "Players Wins!";
-//         }  else if (computerChoice == "rock" && playerChoice == "scissors"
-//         || computerChoice == "paper" && playerChoice == "rock" 
-//         || computerChoice == "scissors" && playerChoice == "paper") {
-//            result = "Computer Wins!";
-//         }; 
-   
-//     return result;
-// };
-//     return result;
-// };
-
-// function game() {
-//     let i;
-
-//     for(i = 0; i < 5; i++) {
-//         let result = playRound();
-//         console.log(result);
-//     }
-    
-// }
-
 // ui implementation identifiers 
-    const main = document.getElementById('main');
+const main = document.getElementById('main');
 
-    const gameBox = document.getElementById('gameBox');
+const gameBox = document.getElementById('gameBox');
 
-// ui implementation functions 
+
+
 
 // implement greeting screen ui 
 
@@ -159,7 +42,7 @@ function implementGreet() {
 greetScreen();
 
 
-
+// implement gameplay screen
 function implementGame() {
     setTimeout(() => {
     let h1 = document.querySelector('h1');
@@ -170,11 +53,85 @@ function implementGame() {
     for(i = 0; i < playList.length; ++i) {
         let gameChoice = document.createElement('div');
         gameChoice.classList.add(playList[i]);
-        gameChoice.classList.add('gameCircle')
+        gameChoice.classList.add('gameCircle');
+        gameChoice.setAttribute('data-choice', playList[i])
+        gameChoice.addEventListener("click", getData);
         gameBox.appendChild(gameChoice);
     }
    }, 15000);
 };
+
+
+
+// gameplay functions
+function getRandomNum() {
+   let randNum = Math.floor(Math.random() * 3);
+   return randNum;
+}
+
+
+function getComputerChoice() {
+    let randNum = getRandomNum();
+    let answer;
+    if(randNum == 0) {
+        answer = "rock";
+    } else if (randNum == 1) {
+        answer = "paper";
+    } else if (randNum == 2) {
+        answer = "scissors";
+    }
+        return answer;
+    };
+
+function getPlayerSelection() {
+     let playerChoice; 
+     playerChoice = getData();
+    return playerChoice
+}
+
+function getData(e) {
+  let data;
+  data = e.target.getAttribute('data-choice');
+  console.log(data)
+;  return data;
+}
+
+
+
+function playRound() {
+   let result;
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerSelection();
+
+        if(computerChoice == playerChoice) {
+            result = "Tie";
+        } else if (computerChoice == "rock" && playerChoice == "paper"
+         || computerChoice == "paper" && playerChoice == "scissors" 
+         || computerChoice == "scissors" && playerChoice == "rock") {
+            result = "Players Wins!";
+        }  else if (computerChoice == "rock" && playerChoice == "scissors"
+        || computerChoice == "paper" && playerChoice == "rock" 
+        || computerChoice == "scissors" && playerChoice == "paper") {
+           result = "Computer Wins!";
+        }; 
+     
+    return result;
+};
+
+function game() {
+    let i;
+
+    for(i = 0; i < 5; i++) {
+        let result = playRound();
+        console.log(result);
+    }
+    
+}
+
+
+
+// ui implementation functions 
+
 
 
   
